c2aa4b2e92fb3027b3a1ed77b2d1b822e4ea7ae0
Joe Kramer <joekramer1125@gmail.com>
2017-06-26 13:08:42 -0700

    add App class to contain main method, this will serve as our UI class

---
+++ App.java
@@ -0,0 +1,33 @@
+import java.io.Console;
+
+public class App {
+  public static void main(String[] args) {
+    Vehicle hatchback = new Vehicle();
+    hatchback.mYear = 1994;
+    hatchback.mBrand = "Subaru";
+    hatchback.mModel = "Legacy";
+    hatchback.mMiles = 170000;
+    hatchback.mPrice = 4000;
+
+    Vehicle suv = new Vehicle();
+    suv.mYear = 2002;
+    suv.mBrand = "Ford";
+    suv.mModel = "Explorer";
+    suv.mMiles = 100000;
+    suv.mPrice = 7000;
+
+    Vehicle sedan = new Vehicle();
+    sedan.mYear = 2015;
+    sedan.mBrand = "Toyota";
+    sedan.mModel = "Camry";
+    sedan.mMiles = 50000;
+    sedan.mPrice = 30000;
+
+    Vehicle truck = new Vehicle();
+    truck.mYear = 1999;
+    truck.mBrand = "Ford";
+    truck.mModel = "Ranger";
+    truck.mMiles = 100000;
+    truck.mPrice = 4000;
+  }
+}
